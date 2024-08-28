c34014fe16b016282eb31bbeefa867a99a25654d
Yellam Jhanvi <yellam.jhanvi@ukg.com>
2024-08-28 14:37:59 +0530

    first commit

---
+++ src/main/java/com/yj/cicd_demo/controller/DemoRestController.java
@@ -0,0 +1,21 @@
+package com.yj.cicd_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoRestController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
+
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/jhanvi-07/cicd-demo.git
+//git push -u origin main
